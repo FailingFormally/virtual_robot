d7f009065f2795de5bd89a7670eb5b1f2dfc1adf
Bill Kidwell <46457910+billkidwell@users.noreply.github.com>
2026-07-14 17:35:59 -0400

    Configure field for Wayfinder challenge.

--- Controller/src/virtual_robot/config/Config.java
+++ Controller/src/virtual_robot/config/Config.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.hardware.configuration.MotorType;
 import javafx.scene.image.Image;
 import virtual_robot.controller.Game;
 import virtual_robot.controller.VirtualBot;
-import virtual_robot.games.Decode;
+import virtual_robot.games.NoGame;
 import virtual_robot.robots.classes.MecanumBot;
 
 /**
@@ -32,7 +32,7 @@ public class Config {
     /**
      * The image object for the field.
      */
-    public static final Image BACKGROUND = new Image("/virtual_robot/assets/decode648.bmp");
+    public static final Image BACKGROUND = new Image("/virtual_robot/assets/biobuzz_skillbuilders_wayfinder_648.bmp");
 
     /**
      * If true, Virtual Gamepad joysticks and triggers will stay in the position where they were
@@ -47,7 +47,7 @@ public class Config {
      * Define the game.  This must match the forGame attribute of the GameElementConfig annotation
      * on the VirtualGameElement implementations.
      */
-    public static final Game GAME = new Decode();
+    public static final Game GAME = new NoGame();
 
     /**
      * Friction coefficient between field and robot wheels. A very high value will minimize the effect
